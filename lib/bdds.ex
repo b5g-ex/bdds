81cf051555fa859cc28166f0915e045a9250af75
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-01-06 21:42:27 +0900

    feat: first commit

---
+++ lib/bdds.ex
@@ -0,0 +1,18 @@
+defmodule Bdds do
+  @moduledoc """
+  Documentation for `Bdds`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bdds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bdds_test.exs
@@ -0,0 +1,8 @@
+defmodule BddsTest do
+  use ExUnit.Case
+  doctest Bdds
+
+  test "greets the world" do
+    assert Bdds.hello() == :world
+  end
+end
