58d47eba8ee7b591b98c1b43e50a0cf11872f5e5
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-01-08 20:54:05 +0900

    feat: add elixir_make for Makefile
          we can check its behaviour by `mix compile` and `mix clean`

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Bdds.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:elixir_make | Mix.compilers()],
+      make_targets: ["all"],
+      make_clean: ["clean"]
     ]
   end
 
@@ -21,6 +24,7 @@ defmodule Bdds.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:elixir_make, "~> 0.6", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
